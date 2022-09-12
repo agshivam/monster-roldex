a22e203a4adc49d823c799568ae0c9b69805ec32
Shivam Agarwal <shivam.a.agarwal@kipi.bi>
2022-09-12 12:51:40 +0530

    added new origin

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import './App.css';
 
 const App = () => {
   const [searchField, setSearchField] = useState('');
+  const [title, setTitle] = useState('')
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilterMonsters] = useState(monsters);
+
+  console.log('rendered');
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
@@ -28,9 +31,21 @@ const App = () => {
     setSearchField(searchFieldString);
   };
 
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
+
   return (
     <div className='App'>
-      <h1 className='app-title'>Monsters Rolodex</h1>
+      <h1 className='app-title'>{title}</h1>
+
+      <SearchBox
+        className='title-search-box'
+        onChangeHandler={onTitleChange}
+        placeholder='set Title'
+      />
+      <br/>
 
       <SearchBox
         className='monsters-search-box'
